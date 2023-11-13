63e398ef2f0bf2b9a1b74618ab6b83afc7d4a2ac
Jonas Ackermann <ackermann@projktionisten.de>
2023-11-13 12:20:00 +0100

    M neovim/lua/nuni/remap.lua

--- neovim/lua/nuni/remap.lua
+++ neovim/lua/nuni/remap.lua
@@ -10,7 +10,7 @@ vim.keymap.set("n", "d", '"_d')
 vim.keymap.set("n", "D", '"_D')
 vim.keymap.set("v", "d", '"_d')
 vim.keymap.set("n", "<leader>d", '""d')
-vim.keymap.set("n", "<leader>d", '""d')
+vim.keymap.set("n", "<leader>D", '""d')
 nnoremap <leader>d ""d
 nnoremap <leader>D ""D
 vnoremap <leader>d ""d
