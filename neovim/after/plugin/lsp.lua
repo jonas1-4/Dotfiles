c6703f5e0c14da8fb49d569f277eab29f14ae0dc
Jonas Ackermann <ackermann@projktionisten.de>
2023-11-14 07:53:18 +0100

    M neovim/after/plugin/lsp.lua

--- neovim/after/plugin/lsp.lua
+++ neovim/after/plugin/lsp.lua
@@ -93,7 +93,7 @@ require('nvim-treesitter.configs').setup {
 
 nvim_lsp.tsserver.setup{
     on_attach = 
-        on_attach()
+        on_attach
 }
 nvim_lsp.angularls.setup{
     on_attach = on_attach,
