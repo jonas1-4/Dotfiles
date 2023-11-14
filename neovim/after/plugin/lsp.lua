6dc1db66747b8a3da02a6068159c0e323f1add97
Jonas Ackermann <ackermann@projktionisten.de>
2023-11-14 07:53:12 +0100

    M neovim/after/plugin/lsp.lua

--- neovim/after/plugin/lsp.lua
+++ neovim/after/plugin/lsp.lua
@@ -74,9 +74,6 @@ local on_attach = function(client, bufnr)
     if client.name == 'tsserver' then
         client.resolved_capabilities.document_formatting = false
     end
-    if client.name == 'tsserver' then
-        client.resolved_capabilities.document_formatting = false
-    end
 
 end
 
