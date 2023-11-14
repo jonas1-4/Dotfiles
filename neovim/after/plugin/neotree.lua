b8da99014a1872909226ddec5696b62daf162231
Jonas Ackermann <ackermann@projktionisten.de>
2023-11-14 08:44:43 +0100

    M neovim/after/plugin/neotree.lua

--- neovim/after/plugin/neotree.lua
+++ neovim/after/plugin/neotree.lua
@@ -42,9 +42,7 @@ require("neo-tree").setup({
             {
                 event = "file_opened",
                 handler = function(file_path)
-                    -- auto close
                     -- vimc.cmd("Neotree close")
-                    -- OR
                     require("neo-tree.command").execute({ action = "close" })
                 end
             },
