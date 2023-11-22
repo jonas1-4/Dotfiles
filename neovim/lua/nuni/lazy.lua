0070448a4c5e02dcf8da7b3ed0f87b1515e9aa1d
Jonas Ackermann <ackermann@projktionisten.de>
2023-11-22 11:05:12 +0100

    M neovim/lua/nuni/lazy.lua

--- neovim/lua/nuni/lazy.lua
+++ neovim/lua/nuni/lazy.lua
@@ -126,5 +126,13 @@ require("lazy").setup({
                 -- add other required languages here
             }
         }
-    },
+ -- Lua
+{
+  "folke/zen-mode.nvim",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+}   },
 })
