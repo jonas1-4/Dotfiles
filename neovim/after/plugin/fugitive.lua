c3aa5a61fbe5c75fc95c7655bbe54d5a1efe7f27
Jonas Ackermann <ackermann@projktionisten.de>
2023-11-24 13:14:36 +0100

    M neovim/after/plugin/fugitive.lua

--- neovim/after/plugin/fugitive.lua
+++ neovim/after/plugin/fugitive.lua
@@ -4,7 +4,6 @@ local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive
 
 local autocmd = vim.api.nvim_create_autocmd
 autocmd("BufWinEnter", {
-    group = ThePrimeagen_Fugitive,
     pattern = "*",
     callback = function()
         if vim.bo.ft ~= "fugitive" then
