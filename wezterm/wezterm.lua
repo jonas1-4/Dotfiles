2f9a1bb780fe4b9655b5d2d8a8739b6475f1ef78
Jonas Ackermann <ackermann@projktionisten.de>
2024-01-30 11:30:29 +0100

    M wezterm/wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -88,7 +88,6 @@ config.keys = {
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
   local pane = tab.active_pane
   local cwd = pane. current_working_dir
-  local color = "navy"
   if tab.is_active then
     color = "blue"
   end
