2a389ba59a231758898e792471159f76f8769adc
Jonas Ackermann <ackermann@projktionisten.de>
2024-01-30 11:59:43 +0100

    M wezterm/wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -86,7 +86,7 @@ config.keys = {
 }
 
 wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
-    return pane.current_working_dir:match("/(%w+)$")
+    return pane.current_working_dir:match("(.*/)")
 end
 )
 
