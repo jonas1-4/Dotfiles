fb82fe9a705e37772a61cb67cc6b5306bf99a5fd
Jonas Ackermann <ackermann@projktionisten.de>
2024-01-30 13:17:49 +0100

    M wezterm/wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -86,7 +86,7 @@ config.keys = {
 }
 
 local function get_current_working_dir(tab)
-    local current_dir = tab.active_pane.current_working_dir
+    local current_dir = pane.current_working_dir
     local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
 
     return current_dir == HOME_DIR and "." or string.gsub(current_dir, "(.*[/\\])(.*)", "%2")
