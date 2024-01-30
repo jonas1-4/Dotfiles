10011484247f243223a601b1101967d43ddcb740
Jonas Ackermann <ackermann@projktionisten.de>
2024-01-30 11:48:37 +0100

    M wezterm/wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -85,16 +85,6 @@ config.keys = {
     },
 }
 wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
-  local zoomed = ''
-  if tab.active_pane.is_zoomed then
-    zoomed = '[Z] '
-  end
-
-  local index = ''
-  if #tabs > 1 then
-    index = string.format('[%d/%d] ', tab.tab_index + 1, #tabs)
-  end
-
   return pane.current_working_dir
   -- return zoomed .. tab.active_pane.current_working_dir
 end
