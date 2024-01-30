d166b66051437a0e76a49f083623f8ebb11d6e50
Jonas Ackermann <ackermann@projktionisten.de>
2024-01-30 12:18:48 +0100

    M wezterm/wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,131 +1,131 @@
--- Pull in the wezterm API
-local wezterm = require 'wezterm'
-
--- This table will hold the configuration.
-local config = {}
-
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-    config = wezterm.config_builder()
-end
-
-config.font = wezterm.font 'Fira Code'
-config.color_scheme = 'Gruvbox Dark (Gogh)'
-config.colors = {
-    tab_bar = {
-        active_tab = {
-            bg_color = '#282828',
-            fg_color = '#ebdbb2'
-        },
-    }
-}
-config.keys = {
-    -- Turn off the default CMD-m Hide action, allowing CMD-m to
-    -- be potentially recognized and handled by the tab
-    {
-        key = '+',
-        mods = 'CMD',
-        action = wezterm.action.ActivateTab(0),
-    },
-    {
-        key = '[',
-        mods = 'CMD',
-        action = wezterm.action.ActivateTab(1),
-    },
-    {
-        key = '{',
-        mods = 'CMD',
-        action = wezterm.action.ActivateTab(2),
-    },
-    {
-        key = '(',
-        mods = 'CMD',
-        action = wezterm.action.ActivateTab(3),
-    },
-    {
-        key = '&',
-        mods = 'CMD',
-        action = wezterm.action.ActivateTab(4),
-    },
-    {
-        key = '=',
-        mods = 'CMD',
-        action = wezterm.action.ActivateTab(5),
-    },
-    {
-        key = ')',
-        mods = 'CMD',
-        action = wezterm.action.ActivateTab(6),
-    },
-    {
-        key = '}',
-        mods = 'CMD',
-        action = wezterm.action.ActivateTab(7),
-    },
-    {
-        key = ']',
-        mods = 'CMD',
-        action = wezterm.action.ActivateTab(8),
-    },
-    {
-        key = '*',
-        mods = 'CMD',
-        action = wezterm.action.ActivateTab(9),
-    },
-    {
-        key = 'LeftArrow',
-        mods = 'CMD',
-        action = wezterm.action.ActivateTabRelative(-1),
-    },
-    {
-        key = 'RightArrow',
-        mods = 'CMD',
-        action = wezterm.action.ActivateTabRelative(1),
-    },
-}
-
-local function get_current_working_dir(tab)
-    local current_dir = tab.active_pane.current_working_dir
-    local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
-
-    return current_dir == HOME_DIR and "." or string.gsub(current_dir, "(.*[/\\])(.*)", "%2")
-end
-
-
-wezterm.on("format-tab-title", function(tab, tabs, panes, hover, max_width)
-
-
-	local title = string.format(" %s  %s ~ %s  ", "❯", get_current_working_dir(tab))
-
-
-	return {
-		{ "haha" },
-	}
-end)
-
--- Zenmode
-wezterm.on('user-var-changed', function(window, pane, name, value)
-    local overrides = window:get_config_overrides() or {}
-    if name == "ZEN_MODE" then
-        local incremental = value:find("+")
-        local number_value = tonumber(value)
-        if incremental ~= nil then
-            while (number_value > 0) do
-                window:perform_action(wezterm.action.IncreaseFontSize, pane)
-                number_value = number_value - 1
-            end
-            overrides.enable_tab_bar = false
-        elseif number_value < 0 then
-            window:perform_action(wezterm.action.ResetFontSize, pane)
-            overrides.font_size = nil
-            overrides.enable_tab_bar = true
-        else
-            overrides.font_size = number_value
-            overrides.enable_tab_bar = false
-        end
-    end
-    window:set_config_overrides(overrides)
-end)
--- and finally, return the configuration to wezterm
-return config
+-- -- Pull in the wezterm API
+-- local wezterm = require 'wezterm'
+--
+-- -- This table will hold the configuration.
+-- local config = {}
+--
+-- -- In newer versions of wezterm, use the config_builder which will
+-- -- help provide clearer error messages
+-- if wezterm.config_builder then
+--     config = wezterm.config_builder()
+-- end
+--
+-- config.font = wezterm.font 'Fira Code'
+-- config.color_scheme = 'Gruvbox Dark (Gogh)'
+-- config.colors = {
+--     tab_bar = {
+--         active_tab = {
+--             bg_color = '#282828',
+--             fg_color = '#ebdbb2'
+--         },
+--     }
+-- }
+-- config.keys = {
+--     -- Turn off the default CMD-m Hide action, allowing CMD-m to
+--     -- be potentially recognized and handled by the tab
+--     {
+--         key = '+',
+--         mods = 'CMD',
+--         action = wezterm.action.ActivateTab(0),
+--     },
+--     {
+--         key = '[',
+--         mods = 'CMD',
+--         action = wezterm.action.ActivateTab(1),
+--     },
+--     {
+--         key = '{',
+--         mods = 'CMD',
+--         action = wezterm.action.ActivateTab(2),
+--     },
+--     {
+--         key = '(',
+--         mods = 'CMD',
+--         action = wezterm.action.ActivateTab(3),
+--     },
+--     {
+--         key = '&',
+--         mods = 'CMD',
+--         action = wezterm.action.ActivateTab(4),
+--     },
+--     {
+--         key = '=',
+--         mods = 'CMD',
+--         action = wezterm.action.ActivateTab(5),
+--     },
+--     {
+--         key = ')',
+--         mods = 'CMD',
+--         action = wezterm.action.ActivateTab(6),
+--     },
+--     {
+--         key = '}',
+--         mods = 'CMD',
+--         action = wezterm.action.ActivateTab(7),
+--     },
+--     {
+--         key = ']',
+--         mods = 'CMD',
+--         action = wezterm.action.ActivateTab(8),
+--     },
+--     {
+--         key = '*',
+--         mods = 'CMD',
+--         action = wezterm.action.ActivateTab(9),
+--     },
+--     {
+--         key = 'LeftArrow',
+--         mods = 'CMD',
+--         action = wezterm.action.ActivateTabRelative(-1),
+--     },
+--     {
+--         key = 'RightArrow',
+--         mods = 'CMD',
+--         action = wezterm.action.ActivateTabRelative(1),
+--     },
+-- }
+--
+-- local function get_current_working_dir(tab)
+--     local current_dir = tab.active_pane.current_working_dir
+--     local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
+--
+--     return current_dir == HOME_DIR and "." or string.gsub(current_dir, "(.*[/\\])(.*)", "%2")
+-- end
+--
+--
+-- wezterm.on("format-tab-title", function(tab, tabs, panes, hover, max_width)
+--
+--
+-- 	local title = string.format(" %s  %s ~ %s  ", "❯", get_current_working_dir(tab))
+--
+--
+-- 	return {
+-- 		{ "haha" },
+-- 	}
+-- end)
+--
+-- -- Zenmode
+-- wezterm.on('user-var-changed', function(window, pane, name, value)
+--     local overrides = window:get_config_overrides() or {}
+--     if name == "ZEN_MODE" then
+--         local incremental = value:find("+")
+--         local number_value = tonumber(value)
+--         if incremental ~= nil then
+--             while (number_value > 0) do
+--                 window:perform_action(wezterm.action.IncreaseFontSize, pane)
+--                 number_value = number_value - 1
+--             end
+--             overrides.enable_tab_bar = false
+--         elseif number_value < 0 then
+--             window:perform_action(wezterm.action.ResetFontSize, pane)
+--             overrides.font_size = nil
+--             overrides.enable_tab_bar = true
+--         else
+--             overrides.font_size = number_value
+--             overrides.enable_tab_bar = false
+--         end
+--     end
+--     window:set_config_overrides(overrides)
+-- end)
+-- -- and finally, return the configuration to wezterm
+-- return config
