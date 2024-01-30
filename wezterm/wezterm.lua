54f3ee35b2856c15c4c7bbd4d5a3fba0f2acbfe4
Jonas Ackermann <ackermann@projktionisten.de>
2024-01-30 12:21:47 +0100

    M wezterm/wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -111,7 +111,7 @@ wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
     index = string.format('[%d/%d] ', tab.tab_index + 1, #tabs)
   end
 
-	local title = string.format(" %s  %s ~ %s  ", "❯", get_current_working_dir(tab))
+  local title = string.format(" %s  %s ~ %s  ", "❯", get_current_working_dir(tab))
   return zoomed .. index  
 end)
 -- Zenmode
