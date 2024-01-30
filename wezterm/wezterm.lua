865e77f1be326e17cf737f5db9da714a04727241
Jonas Ackermann <ackermann@projktionisten.de>
2024-01-30 12:17:16 +0100

    M wezterm/wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -85,6 +85,12 @@ config.keys = {
     },
 }
 
+local function get_current_working_dir(tab)
+	local current_dir = tab.active_pane.current_working_dir
+	local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
+
+	return current_dir == HOME_DIR and "." or string.gsub(current_dir, "(.*[/\\])(.*)", "%2")
+end
 wezterm.on('format-window-title', function(tab, pane, tabs, panes)
     local title = string.format(" %s  %s ~ %s  ", "❯", get_current_working_dir(pane))
 
