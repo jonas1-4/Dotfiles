8a31452eece15d131e472479b25fb654cd8f92b8
Jonas Ackermann <ackermann@projktionisten.de>
2024-01-30 12:53:55 +0100

    M wezterm/wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -87,7 +87,7 @@ config.keys = {
 
 wezterm.on('format-window-title', function(tab, pane, tabs, panes, config )
 
-
+	local title = string.format(" %s  %s ~ %s  ", "❯", get_current_working_dir(tab))
 
 	return "haha"
 end)
