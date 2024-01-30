0329303e16bd83b71669db3063de2fcb900177f3
Jonas Ackermann <ackermann@projktionisten.de>
2024-01-30 12:21:18 +0100

    M wezterm/wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -100,7 +100,19 @@ wezterm.on('format-window-title', function(tab, pane, tabs, panes, config )
 
 	return "haha"
 end)
+wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
+  local zoomed = ''
+  if tab.active_pane.is_zoomed then
+    zoomed = '[Z] '
+  end
 
+  local index = ''
+  if #tabs > 1 then
+    index = string.format('[%d/%d] ', tab.tab_index + 1, #tabs)
+  end
+
+  return zoomed .. index .. tab.active_pane.title
+end)
 -- Zenmode
 wezterm.on('user-var-changed', function(window, pane, name, value)
     local overrides = window:get_config_overrides() or {}
