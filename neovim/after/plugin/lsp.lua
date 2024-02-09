ecea3b26dc0394980032c013629d5ba3d3a3ca6e
Jonas Ackermann <ackermann@projktionisten.de>
2024-02-09 11:55:02 +0100

    M neovim/after/plugin/lsp.lua

--- neovim/after/plugin/lsp.lua
+++ neovim/after/plugin/lsp.lua
@@ -21,7 +21,7 @@ local on_attach = function(client, bufnr)
 end
 
 -- Use a loop to conveniently setup multiple servers
-local servers = { 'html', 'cssls', 'bashls', 'jsonls', 'intelephense', 'lua_ls', 'tsserver', 'angularls', }
+local servers = { 'html', 'cssls', 'bashls', 'jsonls', 'intelephense', 'lua_ls', '', 'angularls', }
 -- 'dartls'
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup { on_attach = on_attach }
