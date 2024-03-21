a953e6788e17543445ebc2e14028309429792ab3
Jonas Ackermann <ackermann@projktionisten.de>
2024-03-21 09:35:39 +0100

    M neovim/after/plugin/lsp.lua

--- neovim/after/plugin/lsp.lua
+++ neovim/after/plugin/lsp.lua
@@ -5,12 +5,12 @@
 local nvim_lsp = require('lspconfig')
 
 nvim_lsp.eslint.setup({
-    on_attach = function(client, bufnr)
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            command = "EslintFixAll",
-        })
-    end,
+    -- on_attach = function(client, bufnr)
+    --     vim.api.nvim_create_autocmd("BufWritePre", {
+    --         buffer = bufnr,
+    --         command = "EslintFixAll",
+    --     })
+    -- end,
 })
 
 local on_attach = function(client, bufnr)
