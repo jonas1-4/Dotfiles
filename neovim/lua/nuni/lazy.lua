2aad3edd27cb63c30ef701cb02918efb41a7a726
Jonas Ackermann <ackermann@projktionisten.de>
2024-04-17 16:09:29 +0200

    M neovim/lua/nuni/lazy.lua

--- neovim/lua/nuni/lazy.lua
+++ neovim/lua/nuni/lazy.lua
@@ -3,9 +3,6 @@ require("lazy").setup({
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
   }
 }
         -- morhetz/gruvbox config
