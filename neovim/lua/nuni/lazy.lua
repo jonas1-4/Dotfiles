5ac53a6e868d232b840abe694944e56f43481339
Jonas Ackermann <ackermann@projktionisten.de>
2024-04-17 16:09:37 +0200

    M neovim/lua/nuni/lazy.lua

--- neovim/lua/nuni/lazy.lua
+++ neovim/lua/nuni/lazy.lua
@@ -1,10 +1,10 @@
 require("lazy").setup({
-{
-  "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-  }
-}
+        {
+            "folke/todo-comments.nvim",
+            dependencies = { "nvim-lua/plenary.nvim" },
+            opts = {
+            }
+        },
         -- morhetz/gruvbox config
         {
             'ellisonleao/gruvbox.nvim',
