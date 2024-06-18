c9d90ec6fade94544ef9efd1944b41b87da51afc
Jonas Ackermann <ackermann@projktionisten.de>
2024-06-18 12:20:32 +0200

    M neovim/after/plugin/trouble.lua

--- neovim/after/plugin/trouble.lua
+++ neovim/after/plugin/trouble.lua
@@ -1,5 +1,5 @@
 -- Lua
-vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+vim.keymap.set("n", "<leader>xx", "<cmd>Trouble :<cr>",
   {silent = true, noremap = true}
 )
 vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
