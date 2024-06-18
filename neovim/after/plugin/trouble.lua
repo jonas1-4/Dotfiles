65fe41ee4164a5eb7ccd8d783313048f86d03ae5
Jonas Ackermann <ackermann@projktionisten.de>
2024-06-18 12:28:08 +0200

    M neovim/after/plugin/trouble.lua

--- neovim/after/plugin/trouble.lua
+++ neovim/after/plugin/trouble.lua
@@ -2,18 +2,18 @@
 vim.keymap.set("n", "<leader>xx", "<cmd>Trouble loclist toggle focus=true<cr>",
   {silent = true, noremap = true}
 )
-vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
-  {silent = true, noremap = true}
-)
-vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
-  {silent = true, noremap = true}
-)
-vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
-  {silent = true, noremap = true}
-)
-vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
-  {silent = true, noremap = true}
-)
-vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
-  {silent = true, noremap = true}
-  )
+-- vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+--   {silent = true, noremap = true}
+-- )
+-- vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
+--   {silent = true, noremap = true}
+-- )
+-- vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
+--   {silent = true, noremap = true}
+-- )
+-- vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+--   {silent = true, noremap = true}
+-- )
+-- vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
+--   {silent = true, noremap = true}
+--   )
